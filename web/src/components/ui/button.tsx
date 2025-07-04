b899319981a4f32f49062d015e3a9e64a0703c06
Matheus Vieira <matheusvieira1900@hotmail.com>
2025-07-04 00:26:58 -0300

    feat(web): configure inital components and design

---
+++ web/src/components/ui/button.tsx
@@ -0,0 +1,31 @@
+import type { ComponentProps } from "react";
+import { tv, type VariantProps } from "tailwind-variants";
+
+const buttonVariants = tv({
+  base: "cursor-pointer w-full max-w-[352px] text-md disabled:opacity-50",
+
+  variants: {
+    theme: {
+      primary:
+        "text-white bg-blue-base rounded-lg py-[15px] hover:bg-blue-dark",
+      secondary:
+        "hover:border-blue-dark  rounded-sm bg-gray-200 p-[8px] font-semibold text-gray-500 hover:border",
+    },
+  },
+
+  defaultVariants: {
+    theme: "primary",
+  },
+});
+
+type TButtonProps = ComponentProps<"button"> &
+  VariantProps<typeof buttonVariants>;
+
+export function Button({ theme, className, ...props }: TButtonProps) {
+  return (
+    <button
+      className={buttonVariants({ theme, className })}
+      {...props}
+    ></button>
+  );
+}
---
+++ web/src/components/ui/input-text.tsx
@@ -0,0 +1,42 @@
+import { type ComponentProps } from "react";
+import { tv, type VariantProps } from "tailwind-variants";
+
+const inputVariants = tv({
+  base: "placeholder:text-md w-full max-w-[352px] rounded-md border border-gray-300 px-[16px] py-[12px] text-gray-600 outline-none placeholder:text-gray-400",
+
+  variants: {
+    status: {
+      active: "border-blue-base border-[1.5px]",
+      error: "border-danger border-[1.5px]",
+    },
+  },
+});
+
+const labelVariants = tv({
+  base: "text-xs uppercase font-light text-gray-500",
+  variants: {
+    status: {
+      active: "text-blue-base font-bold",
+      error: "text-danger font-bold",
+    },
+  },
+});
+
+type TButtonProps = ComponentProps<"input"> &
+  VariantProps<typeof inputVariants> & {
+    label: string;
+  };
+
+export function InputText({ status, label, ...props }: TButtonProps) {
+  return (
+    <label className="flex flex-col gap-[6px]">
+      <span className={labelVariants({ status })}>{label}</span>
+
+      <input
+        type="text"
+        className={inputVariants({ status })}
+        {...props}
+      ></input>
+    </label>
+  );
+}

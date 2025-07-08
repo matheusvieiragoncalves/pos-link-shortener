f0c61750e83e708f2c919e8bfa074c9103625434
Matheus Vieira <matheusvieira1900@hotmail.com>
2025-07-08 20:08:33 -0300

    feat(web): customize scroll in list

--- web/src/components/link-list.tsx
+++ web/src/components/link-list.tsx
@@ -1,8 +1,8 @@
 import { CopyIcon } from "@phosphor-icons/react";
+import * as ScrollArea from "@radix-ui/react-scroll-area";
 import { useEffect, useRef } from "react";
 import { useLinks } from "../store/links";
 import { LinkItem } from "./link-item";
-import { LinkListEmpty } from "./link-list-empty";
 import { Button } from "./ui/button";
 import { LoadingBar } from "./ui/loading-bar";
 
@@ -57,13 +57,8 @@ export function LinkList() {
         </Button>
       </div>
 
-      <div
-        ref={scrollContainerRef}
-        className="max-h-[40dvh] overflow-y-auto md:max-h-[78dvh]"
-      >
-        {!links.size ? (
-          <LinkListEmpty />
-        ) : (
+      <ScrollArea.Root type="hover">
+        <ScrollArea.Viewport className="max-h-[40dvh] pr-2 md:max-h-[78dvh]">
           <>
             {Array.from(links.entries()).map(([id, link]) => (
               <LinkItem key={id} {...link} />
@@ -77,8 +72,14 @@ export function LinkList() {
               </div>
             )}
           </>
-        )}
-      </div>
+        </ScrollArea.Viewport>
+        <ScrollArea.Scrollbar
+          orientation="vertical"
+          className="flex w-1 rounded-full bg-gray-300 transition-opacity duration-200 ease-in-out"
+        >
+          <ScrollArea.Thumb className="flex-1 rounded-full bg-gray-500" />
+        </ScrollArea.Scrollbar>
+      </ScrollArea.Root>
     </div>
   );
 }
